efb75403055bc5997f725c912c8e371faa99ce57
gioanbatan <giovannibassani@hotmail.com>
2022-10-10 15:21:07 +0200

    Text structure created

--- js/script.js
+++ js/script.js
@@ -1,1 +1,36 @@
-alert();
+// **Consegna:**
+//  Scrivi un programma che stampi in console i numeri da 1 a 100,
+//  ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”.
+//  Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+
+//  **Prima di partire a scrivere codice poniamoci qualche domanda:**
+ 
+//  Come faccio a sapere se un numero è divisibile per un altro?
+//  Abbiamo visto qualcosa di particolare che possiamo usare?
+//  **Consigli del giorno:**
+//  1. Scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
+//  2. Proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
+// **BONUS 1:**
+//  Crea un container nel DOM , aggiungendo (attraverso la funzione `append()`) un elemento html con il numero o la stringa corretta da mostrare.
+// **BONUS 2:**
+//  Applica stili differenti agli elementi aggiunti al DOM nel *BONUS 1*, a seconda che il valore inserito sia un numero, un fizz, un buzz o un fizzbuzz.
+//  Se sei a corto di idee per lo stile, potresti prendere spunto dallo screenshot fornito in consegna.
+// Buon lavoro e buon divertimento! :faccia_leggermente_sorridente:
+
+// INPUT
+
+// ELABORAZIONE
+// Ciclo che scorre i numeri da 1 a 100
+
+// Per ogni numero del ciclo viene creata un div 'cella' numerata
+
+// SE il numero esaminato è divisibile per 3 senza resto viene assegnata la classe fizz alla cella
+
+// SE il numero esaminato è divisibile per 5 senza resto viene assegnata la classe buzz alla cella
+
+// SE il numero esaminato è divisibile per 3 e 5 senza resto viene asseganta la classe bothzz alla cella
+
+// Il ciclo termina superato 100
+
+// OUTPUT
+// Viene composta serie di celle nell'html
